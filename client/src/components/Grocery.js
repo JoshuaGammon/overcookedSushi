1942694d905dfbc1ebf58f67391775b2aef6eaa8
oliviabangs <obangs@trinity.edu>
2023-11-29 11:29:03 -0600

    Grocery List displaying

--- client/src/components/Grocery.js
+++ client/src/components/Grocery.js
@@ -3,9 +3,33 @@ import Axios from 'axios' //  I'm not sure if we need Axios
 //import {useHistory} from "react-router-dom"
 
 function Grocery() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:8081/retrieveGrocery')
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err));
+  }, [])
+
   return(
     <>
       <p>Hello this is your grocery list!</p>
+      <table>
+        <thead>
+          <th>Ingredient Name</th>
+          <th>Quantity</th>
+          <th>Unit of Measurement</th>
+        </thead>
+        <tbody>
+          {data.map((rowData, rowIndex) => (
+            <tr key={rowIndex}>
+              <td>{rowData.ingredient_name}</td>
+              <td>{rowData.quantity_numerator + "/" + rowData.quantity_denominator}</td>
+              <td>{rowData.measurement_type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 };
--- server/index.js
+++ server/index.js
@@ -12,12 +12,20 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '', // FILL IN
-    database: 'test_db' // FILL IN
+    database: '' // FILL IN
 })
 
 //  API Calls
-app.get('/users', (req,res)=>{
-    const sql = "SELECT * FROM test_table"
+app.get('/retrieveUsers', (req,res)=>{
+    const sql = "SELECT * FROM User"
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/retrieveGrocery', (req,res)=>{
+    const sql = "SELECT * FROM Grocery_List, Ingredient WHERE Grocery_List.username = 'juliac' AND Grocery_List.ingredient_id = Ingredient.ingredient_id"
     db.query(sql, (err,data)=>{
         if(err) return res.json(err);
         return res.json(data);
