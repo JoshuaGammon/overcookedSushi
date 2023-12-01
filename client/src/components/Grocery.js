f4d2abfcb613df731167fad764c20362dc581a0f
oliviabangs <obangs@trinity.edu>
2023-12-01 11:32:42 -0600

    Added SQL files, implemented grocery list front end, added DB calls for pulling grocery list and moving grocery list items into the pantry

--- client/src/components/Grocery.js
+++ client/src/components/Grocery.js
@@ -9,29 +9,55 @@ function Grocery() {
     .then(res => res.json())
     .then(data => setData(data))
     .catch(err => console.log(err));
-  }, [])
+  }, []);
 
-  return(
-    <>
-      <p>Hello this is your grocery list!</p>
-      <table>
-        <thead>
-          <th>Ingredient Name</th>
-          <th>Quantity</th>
-          <th>Unit of Measurement</th>
-        </thead>
-        <tbody>
-          {data.map((rowData, rowIndex) => (
-            <tr key={rowIndex}>
-              <td>{rowData.ingredient_name}</td>
-              <td>{rowData.quantity_numerator + "/" + rowData.quantity_denominator}</td>
-              <td>{rowData.measurement_type}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  )
+  if(data.length > 0) {
+    return(
+      <>
+        <p>Hello this is your grocery list!</p>
+        <div>
+            {data.map((rowData, rowIndex) => (
+                <table>
+                  <thead>
+                    <th>Ingredient Name</th>
+                    <th>Quantity</th>
+                    <th>Unit of Measurement</th>
+                  </thead>
+                  <tbody>
+                    <tr key={rowIndex}>
+                      <td>{rowData.ingredient_name}</td>
+                      <td>{rowData.quantity_numerator + "/" + rowData.quantity_denominator}</td>
+                      <td>{rowData.measurement_type}</td>
+                    </tr>
+                  </tbody>
+                </table>
+            ))}
+        </div>
+        <button onClick = {() => migrateGroceries()}>Mark Items as Bought</button>
+      </>
+    )
+  }
+  else {
+    return(
+      <>
+        <p>Your grocery list is empty.</p>
+      </>
+    )
+  }
 };
+
+function migrateGroceries() {
+  fetch('http://localhost:8081/migrateGroceries')
+  .then(res => res.json())
+  .then(data => {
+    if(data){
+      console.log(data);
+    }
+    else {
+      console.log("Failed to migrate groceries");
+    }
+  })
+  .catch(err => console.log(err));
+}
 
 export default Grocery;
---
+++ server/SQL Files/UserCreation.sql
@@ -0,0 +1,19 @@
+INSERT INTO Pantry VALUES('juliac', 1, CURDATE(), 8, 1, 'Cup(s)');
+INSERT INTO Pantry VALUES('juliac', 24, CURDATE(), 2, 1, 'Cup(s)');
+INSERT INTO Pantry VALUES('juliac', 27, CURDATE(), 1, 1, 'Unit(s)');
+INSERT INTO Pantry VALUES('juliac', 32, CURDATE(), 20, 1, 'Tablespoon(s)');
+INSERT INTO Pantry VALUES('juliac', 37, CURDATE(), 1, 1, 'Unit(s)');
+INSERT INTO Pantry VALUES('juliac', 38, CURDATE(), 4, 1, 'Unit(s)');
+INSERT INTO Pantry VALUES('juliac', 41, CURDATE(), 16, 1, 'Ounce(s)');
+INSERT INTO Pantry VALUES('juliac', 41, CURDATE(), 16, 1, 'Ounce(s)');
+INSERT INTO Pantry VALUES('juliac', 61, CURDATE(), 4, 1, 'Cup(s)');
+INSERT INTO Pantry VALUES('juliac', 103, CURDATE(), 8, 1, 'Tablespoon(s)');
+INSERT INTO Pantry VALUES('juliac', 126, CURDATE(), 8, 1, 'Teaspoon(s)');
+INSERT INTO Pantry VALUES('juliac', 134, CURDATE(), 10, 1, 'Tablespoon(s)');
+INSERT INTO Pantry VALUES('juliac', 42, CURDATE(), 2, 1, 'Tablespoon(s)');
+INSERT INTO Pantry VALUES('juliac', 43, CURDATE(), 1, 1, 'Pound(s)');
+INSERT INTO Pantry VALUES('juliac', 49, CURDATE(), 2, 1, 'Teaspoon(s)');
+INSERT INTO Pantry VALUES('juliac', 69, CURDATE(), 2, 1, 'Tablespoon(s)');
+
+INSERT INTO Pantry VALUES('juliac', 94, CURDATE(), 1, 1, 'Pound(s)');
+INSERT INTO Pantry VALUES('juliac', 95, CURDATE(), );
